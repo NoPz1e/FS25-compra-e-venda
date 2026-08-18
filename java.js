b75f0c6bb4634075ca8b0813e759f7b2e5f1d421
NoPz1e <cristianomanuelneto2006@gmail.com>
2026-08-18 11:41:56 +0100

    falta calculo trator

--- java.js
+++ java.js
@@ -1,32 +1,31 @@
 const select = document.getElementById('tipoSelect');
 const blocos = document.querySelectorAll('.campos-condicionais');
 
-// charrua
-const charruaWidth = document.getElementById('charrua_width');
-const precocharrua = document.getElementById('preco_charrua');
-
-const preco_m_charrua = 6400; // define aqui o valor pelo qual queres multiplicar
-
-
-
-
 
 select.addEventListener('change', function() {
   // Esconde todos os blocos primeiro
   blocos.forEach(bloco => bloco.hidden = true);
 
   // Mostra apenas o bloco correspondente à opção escolhida
-  if (select.value === 'charrua') {
-    document.getElementById('charrua').hidden = false;
+  if (select.value === 'trator') {
+    document.getElementById('trator').hidden = false;
   } else if (select.value === 'reboque') {
     document.getElementById('campoReboque').hidden = false;
-  } else if (select.value === 'trator') {
-    document.getElementById('campoTrator').hidden = false;
+  } else if (select.value === 'charrua') {
+    document.getElementById('charrua').hidden = false;
   }
 
   console.log(select.value)
 });
 
+
+
+
+// charrua
+const charruaWidth = document.getElementById('charrua_width');
+const precocharrua = document.getElementById('preco_charrua');
+
+const preco_m_charrua = 6400; // define aqui o valor pelo qual queres multiplicar
 
 charruaWidth.addEventListener('input', function() {
   const largura = parseFloat(charruaWidth.value);
@@ -38,6 +37,53 @@ charruaWidth.addEventListener('input', function() {
     precocharrua.value = '';
   }
 });
+
+
+// trator
+const cavalagemtrator = document.getElementById('cavalagem_trator');
+const selectCaixaTrator = document.getElementById('caixa_trator');
+const selectVelocidadeTrator = document.getElementById('velocidade_trator');
+const selectMarcaTrator = document.getElementById('marca_trator');
+const selectTamanhoTrator = document.getElementById('tamanho_trator');
+const bracos = document.getElementById('bracos_trator');
+const carregador = document.getElementById('carregador_trator');
+
+const precoTrator = document.getElementById('preco_trator');
+
+
+cavalagemtrator.addEventListener('input', calcularPrecoTrator);
+selectCaixaTrator.addEventListener('change', calcularPrecoTrator);
+selectVelocidadeTrator.addEventListener('change', calcularPrecoTrator);
+selectMarcaTrator.addEventListener('change', calcularPrecoTrator);
+selectTamanhoTrator.addEventListener('change', calcularPrecoTrator);
+bracos.addEventListener('change', calcularPrecoTrator);
+carregador.addEventListener('change', calcularPrecoTrator);
+
+function calcularPrecoTrator() {
+  const cavalagem = parseFloat(cavalagemtrator.value);
+
+  console.log('Cavalagem:', cavalagem);
+  console.log('bracos.value', bracos.checked);
+  console.log('carregador.value', carregador.checked);
+
+
+  
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
